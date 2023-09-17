2e003dcfaf0ff3112ad7ce925453da1918420a06
egentilucci <elia.gentilucci@gmail.com>
2023-09-17 23:15:20 +0200

    feat: add WorkoutCard Widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,132 +1,107 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            // Top Part (10% of the screen height)
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              color: Colors.brown,
-              child: Center(
-                child: FractionallySizedBox(
-                  widthFactor: 0.7, // Button width as a fraction of the parent
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    ),
-                    onPressed: () {
-                      // Button onPressed action
-                    },
-                    child: const Text(
-                      'New Routine',
-                      textScaleFactor: 1.25,
-                    ),
-                  ),
-                ),
-              ),
+        child: Center(
+            child: Column(children: [
+          Container(
+            color: Colors.amber,
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Create Workout")),
+              ],
             ),
-            // Bottom Part
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                color: Colors.black,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      const Text(
-                        "My Routines",
-                        textScaleFactor: 2.0,
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      const Divider(
-                        indent: 10.0,
-                        endIndent: 10.0,
-                        color: Colors.white, // You can set the color of the divider
-                        thickness: 1, // You can set the thickness of the divider
-                      ),
-                      Card(
-                        color: Colors.brown,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: <Widget>[
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  const Text(
-                                    'Routine 1',
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'Edit',
-                                      textScaleFactor: 1.5,
-                                      style: TextStyle(
-                                        color: Colors.red,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const Align(
-                                alignment: Alignment.centerLeft,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    '[Exercises in Routine]',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              FractionallySizedBox(
-                                widthFactor: 0.9, // Button width as a fraction of the parent
-                                child: ElevatedButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                                  ),
-                                  onPressed: () {
-                                    // Button onPressed action
-                                  },
-                                  child: const Text(
-                                    'Start',
-                                    textScaleFactor: 1.25,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+          ),
+          Container(
+            color: Colors.black,
+            padding: const EdgeInsets.all(10.0),
+            height: 771,
+            alignment: Alignment.topCenter,
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [WorkoutCard()],
+                    ),
+                    Row(
+                      children: [WorkoutCard()],
+                    ),
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ])),
       ),
+    );
+  }
+}
+
+class WorkoutCard extends StatelessWidget {
+  const WorkoutCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Colors.blue,
+      child: SizedBox(
+          width: 350,
+          height: 130,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Workout_Name'),
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                  minimumSize: Size.zero,
+                                  padding: EdgeInsets.zero),
+                              child: const Text("Edit"))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                // ROW 2
+                const Row(
+                  children: [Text("Exercise 1, Exercise 2, ...")],
+                ),
+                // ROW 3
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: const Text("Start"))
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
